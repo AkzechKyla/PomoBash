57b76915acaf1cdb5ac00f288e11c27f96a6eb09
Kyla <117506093+AkzechKyla@users.noreply.github.com>
2025-03-02 21:58:04 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
+# Clone repository
+git clone git@github.com/AkzechKyla/PomoBash.git
+cd PomoBash
+
 # Make the scripts executable
 chmod +x configure.sh
 chmod +x pomobash.sh
 
 # Install dependency
-sudo apt update
+sudo apt update -y
 sudo apt install espeak
